51abcff576b0cae71687b3536d8c6fa5c8f144a0
Konstantinos Argyriou <constarg@pm.me>
2021-09-05 18:29:56 +0300

    Make the structure.

---
+++ entities/food.c
@@ -0,0 +1,8 @@
+#include <food/food.h>
+
+
+
+void change_position_of(s_food food, short int x, short int y) {
+
+
+}
---
+++ memory/include/mem.h
@@ -0,0 +1,24 @@
+#ifndef TERMINAL_SNAKE_GAME_MEM_H
+#define TERMINAL_SNAKE_GAME_MEM_H 1
+
+#include <malloc.h>
+
+#define ALLOCATE_MEM(VAR, SIZE, SIZE_OF_TYPE) do {  \
+        VAR = calloc(SIZE, SIZE_OF_TYPE);           \
+        if (VAR == NULL) {                          \
+            exit(1);                                \
+        }                                           \
+    } while(0)
+
+#define REALLOCATE_MEM(VAR, NEW_SIZE) do {          \
+        VAR = realloc(VAR, NEW_SIZE);               \
+        if (VAR == NULL) {                          \
+            exit(1);                                \
+        }                                           \
+    } while(0)
+
+
+
+
+
+#endif
