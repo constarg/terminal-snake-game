879c7039ef57803a788e3d816d10912516ba5b42
Konstantinos Argyriou <constarg@pm.me>
2021-09-06 16:16:28 +0300

    Done with the snake.

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <draw_to_screen.h>
 #include <terminal/terminal.h>
 #include <terminal/keystrokes_codes.h>
+#include <snake/snake.h>
 
 int main() {
-    //draw_start_screen();
     start_non_blocking_mode();
 
     int code = get_pressed_key();
@@ -11,6 +11,11 @@ int main() {
     while (code != ENTER) {
         code = get_pressed_key();
     }
+
+    // start the game.
+
+
+
 
     stop_non_blocking_mode();
     return 0;
