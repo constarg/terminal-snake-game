6c22cd875e793a34d48aebe37f8743b1ec03d042
Konstantinos Argyriou <constarg@pm.me>
2021-09-06 16:35:11 +0300

    Done with food.

--- entities/include/snake/snake.h
+++ entities/include/snake/snake.h
@@ -11,6 +11,6 @@ typedef struct g_snake_piece {
 
 } g_snake_piece;
 
-extern void move_snake(g_snake_piece **snake_pieces, size_t snake_size, s_food *food, int direction);
+extern void move_snake(g_snake_piece **snake_pieces, size_t *snake_size, s_food *food, int direction);
 
 #endif
--- main.c
+++ main.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+
 #include <draw_to_screen.h>
 #include <terminal/terminal.h>
 #include <terminal/keystrokes_codes.h>
 #include <snake/snake.h>
 
 int main() {
+    srand(time(NULL));
     start_non_blocking_mode();
 
     int code = get_pressed_key();
