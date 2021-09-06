ee5f3c8942562f1baf29d9bc38970805c389ffe4
Konstantinos Argyriou <rounnus@gmail.com>
2021-09-06 12:56:12 +0300

    Fix center of text.

--- tools/draw_to_screen.c
+++ tools/draw_to_screen.c
@@ -1,12 +1,10 @@
-#include <unistd.h>
+#include <string.h>
 
 #include <draw_to_screen.h>
 #include <terminal/terminal.h>
-#include <mem.h>
-#include <string.h>
 
-#define START_MSG   "Start Game"
-#define ENTER_MSG   "Press Enter"
+
+#define START_MSG   "Press Enter to Play"
 
 
 void draw_start_screen() {
@@ -15,31 +13,9 @@ void draw_start_screen() {
 
     struct winsize terminal_dimensions = get_terminal_dimensions();
 
-    unsigned short text_location_x = terminal_dimensions.ws_col/2;
+    unsigned short text_location_x = (terminal_dimensions.ws_col - strlen(START_MSG))/2;
     unsigned short text_location_y = terminal_dimensions.ws_row/2;
-    text_location_x -= 2;
-    text_location_y -= 3;
 
-    GO_TO(text_location_x - 4, text_location_y);
+    GO_TO(text_location_x, text_location_y);
     printf("%s\n", START_MSG);
-
-    char *enter_msg = NULL;
-    ALLOCATE_MEM(enter_msg, strlen(ENTER_MSG) + strlen("|   ") + 5, sizeof(char));
-    strcpy(enter_msg, "|   ");
-    strcat(enter_msg, ENTER_MSG);
-    strcat(enter_msg, "   ");
-    strcat(enter_msg, "|");
-
-    char *to_draw[3] = {
-            "+-----------------+\n",
-            enter_msg,
-            "+-----------------+\n"
-    };
-
-    for (int draw = 0; draw < 3; draw++) {
-        GO_TO(text_location_x - 8, text_location_y + draw + 1);
-        printf("%s", to_draw[draw]);
-    }
-
-    free(enter_msg);
 }
